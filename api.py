0b117616f1f02ea819349bb7c0a4651df084d947
Florian Rathgeber <florian.rathgeber@gmail.com>
2014-09-16 13:19:17 +0100

    Serve frontend via flask

--- api.py
+++ api.py
@@ -1,10 +1,41 @@
 from os import environ
 
+from flask import send_from_directory
 from eve import Eve
 
 from settings import API_NAME
 
-api = Eve(API_NAME)
+app = Eve(API_NAME)
+
+
+@app.route('/scripts/<path:filename>')
+def scripts(filename):
+    return send_from_directory(app.root_path + '/dist/scripts/', filename)
+
+
+@app.route('/styles/<path:filename>')
+def styles(filename):
+    return send_from_directory(app.root_path + '/dist/styles/', filename)
+
+
+@app.route('/images/<path:filename>')
+def images(filename):
+    return send_from_directory(app.root_path + '/dist/images/', filename)
+
+
+@app.route('/views/<path:filename>')
+def views(filename):
+    return send_from_directory(app.root_path + '/dist/views/', filename)
+
+
+@app.route("/")
+def index():
+    return send_from_directory(app.root_path + '/dist/', 'index.html')
+
+
+@app.route("/favicon.ico")
+def favicon():
+    return send_from_directory(app.root_path + '/dist/', 'favicon.ico')
 
 if __name__ == '__main__':
     # Heroku support: bind to PORT if defined, otherwise default to 5000.
@@ -14,4 +45,4 @@ if __name__ == '__main__':
     else:
         port = 5000
         host = '127.0.0.1'
-    api.run(host=host, port=port)
+    app.run(host=host, port=port)
